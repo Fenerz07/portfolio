cd9e2c8c7fcf097c3d7a259226ac1321cd2c2ca6
hugo <hugom2005@free.fr>
2024-08-04 22:45:10 +0200

    feat:home

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,55 @@
 class Navbar extends HTMLElement {
     constructor() {
         super();
-        const template = document.getElementById('navbar-template').content;
         const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.appendChild(template.cloneNode(true));
+
+        fetch('components/navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                const template = document.createElement('template');
+                template.innerHTML = data;
+                const templateContent = template.content.querySelector('#navbar-template').content;
+                shadowRoot.appendChild(templateContent.cloneNode(true));
+            });
+
+        fetch('styles/navbar.css')
+            .then(response => response.text())
+            .then(css => {
+                const style = document.createElement('style');
+                style.textContent = css;
+                shadowRoot.appendChild(style);
+            });
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     customElements.define('my-navbar', Navbar);
+});
+
+class Home extends HTMLElement {
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+
+        fetch('components/home.html')
+            .then(response => response.text())
+            .then(data => {
+                const template = document.createElement('template');
+                template.innerHTML = data;
+                const templateContent = template.content.querySelector('#home-template').content;
+                shadowRoot.appendChild(templateContent.cloneNode(true));
+            });
+
+        fetch('styles/home.css')
+            .then(response => response.text())
+            .then(css => {
+                const style = document.createElement('style');
+                style.textContent = css;
+                shadowRoot.appendChild(style);
+            });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    customElements.define('my-home', Home);
 });
